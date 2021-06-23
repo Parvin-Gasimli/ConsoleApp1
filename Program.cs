61d5976ef09efede7d559c117a4a0cec780e3b05
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2021-06-23 04:32:57 -0700

    department class

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.Models;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            
+            
+        }
+    }
+}
