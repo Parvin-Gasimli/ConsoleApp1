681689dfc52fb04e7a0a2c4c0dc52a370291ef77
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2021-06-23 04:41:10 -0700

    end of the employye class

---
+++ Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    class Employee
+    {
+        public static int TotalCount = 1000;
+
+        public string No { get; set; }
+        public string FullName { get; set; }
+        public Employee(string name, string surname)
+        {
+            FullName = name + surname;
+        }
+        public string Position { get; set; }
+        public int Salary { get; set; }
+        public string DepartmentName { get; set; }
+        public Employee(string fullname, string position, int salary, string departmentName)
+        {
+            FullName = 
+            Position = position;
+            DepartmentName = departmentName;
+            Salary = salary;
+            TotalCount++;
+           // No = departmentName.Trim().ToUpper()[0].ToString() + departmentName.Trim().ToUpper()[1].ToString() + TotalCount.ToString();
+            No = departmentName.ToString().Trim().ToUpper().Substring(0, 2) + TotalCount.ToString();
+            
+        }
+        public override string ToString()
+        {
+            return $"{FullName},{Salary},{DepartmentName},{Position},{No}";
+        }
+    }  }
